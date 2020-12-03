a0cc4e80586c68fda3d74d4ea18547afa23183bf
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-03 14:18:00 +0900

    feat: <Layout><Seo /></Layout>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 import Footer from './footer'
 import Header from './header'
 
 const Container = styled.div`
   will-change: opacity;
-  opacity: 0;
   box-sizing: border-box;
   width: 100%;
   padding-right: 1rem;
@@ -15,10 +15,18 @@ const Container = styled.div`
   margin-right: auto;
   margin-left: auto;
   color: #212529;
-  transition: opacity 200ms ease-out 300ms;
 
-  .wf-active &,
-  .wf-inactive {
+  html:not(.no-js) & {
+    opacity: 0;
+    transition: opacity 200ms ease-out 300ms;
+  }
+
+  html:not(.no-js).wf-loading & {
+    opacity: 0;
+  }
+
+  html:not(.no-js).wf-active &,
+  html:not(.no-js).wf-inactive & {
     opacity: 1;
   }
 
@@ -47,12 +55,42 @@ const Container = styled.div`
   }
 `
 
+const Seo: React.FC = () => {
+  const noJsClassName = 'no-js'
+
+  useEffect(() => {
+    const $noJs = document.querySelector(`.${noJsClassName}`)
+    if ($noJs) {
+      $noJs.classList.remove(noJsClassName)
+    }
+  })
+
+  return (
+    <Helmet>
+      <html lang="ja" className={noJsClassName} />
+      <script>{`
+        setTimeout(() => (function(d) {
+          var config = {
+            kitId: "${process.env.GATSBY_TYPEKIT_ID}",
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document), 1);
+      `}</script>
+    </Helmet>
+  )
+}
+
 const Layout: React.FC = ({ children }) => (
-  <Container>
-    <Header />
-    <main>{children}</main>
-    <Footer />
-  </Container>
+  <>
+    <Seo />
+    <Container>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </Container>
+  </>
 )
 
 export default Layout
