bbfcf40c94052f7bde3fb1e36b70060aa85c05f1
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-03 18:24:03 +0900

    feat: favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,13 +15,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `View Expander`,
+        short_name: `View Expander`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#212529`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
