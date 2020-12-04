36ee6daec60484bbcf5693afd15c0f7276cf6193
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-04 19:43:18 +0900

    feat: <BlogPost><Header /></BlogPost>

--- src/components/blog-post.tsx
+++ src/components/blog-post.tsx
@@ -13,14 +13,36 @@ const Article = styled.article`
   margin-top: 100px;
 `
 
+const Header = styled.header`
+  h2 {
+    margin-top: 0;
+    margin-bottom: 0;
+    font-size: 1.6rem;
+    font-weight: 500;
+
+    @media (min-width: 992px) {
+      font-size: 2rem;
+    }
+  }
+
+  p {
+    margin-top: 0.5rem;
+    margin-bottom: 0;
+  }
+
+  time {
+    color: #6c757d;
+  }
+`
+
 const BlogPost: React.FC<Props> = ({ body, date, pictures, tags, title }) => (
   <Article>
-    <header>
+    <Header>
       <h2>{title}</h2>
       <p>
         <time dateTime={date}>{date}</time>
       </p>
-    </header>
+    </Header>
     {pictures && pictures.length > 0 ? (
       <ul>
         {pictures.map(item =>
