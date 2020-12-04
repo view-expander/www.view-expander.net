f7188c0c937a6b353eb76e28e364c54e2025ccc7
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-04 11:37:42 +0900

    feat: <Layout><SEO /></Layout>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
 import Footer from './footer'
 import Header from './header'
 import SEO from './seo'
@@ -54,17 +55,31 @@ const Container = styled.div`
 
 const CONTAINER_ID = 'container'
 
-const Layout: React.FC = ({ children }) => (
-  <>
-    <SEO>
-      <noscript>{`<style type="text/css">#${CONTAINER_ID} { opacity: 1 }</style>`}</noscript>
-    </SEO>
-    <Container id={CONTAINER_ID}>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </Container>
-  </>
-)
+const Layout: React.FC = ({ children }) => {
+  const siteMetadata = useSiteMetadata()
+
+  return (
+    <>
+      <SEO>
+        <script>{`
+        setTimeout(() => (function(d) {
+          var config = {
+            kitId: "${siteMetadata?.typekitId}",
+            scriptTimeout: 3000,
+            async: true
+          },
+          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document), 1);
+      `}</script>
+        <noscript>{`<style type="text/css">#${CONTAINER_ID} { opacity: 1 }</style>`}</noscript>
+      </SEO>
+      <Container id={CONTAINER_ID}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </Container>
+    </>
+  )
+}
 
 export default Layout
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -27,16 +27,6 @@ const SEO: React.FC<{ description?: string; title?: string }> = ({
         name="twitter:creator"
         content={siteMetadata?.author || undefined}
       />
-      <script>{`
-        setTimeout(() => (function(d) {
-          var config = {
-            kitId: "${siteMetadata?.typekitId}",
-            scriptTimeout: 3000,
-            async: true
-          },
-          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-        })(document), 1);
-      `}</script>
       {children}
     </Helmet>
   )
