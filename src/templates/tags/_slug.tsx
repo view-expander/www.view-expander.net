6309ab901682f1ae3ce3a4e8bf1cc2e80bff861b
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-05 17:55:54 +0900

    feat: <TagItemsTemplate />

---
+++ src/templates/tags/_slug.tsx
@@ -0,0 +1,102 @@
+import { graphql, Link, PageProps } from 'gatsby'
+import React from 'react'
+import styled from 'styled-components'
+import { TagItemsQuery } from '../../../graphql-types'
+import BlogPost from '../../components/blog-post'
+import EffectedLink from '../../components/effected-link'
+import Layout from '../../components/layout'
+import SEO from '../../components/seo'
+import { useSiteMetadata } from '../../hooks/useSiteMetadata'
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  color: #6c757d;
+`
+
+const Separator = styled.span`
+  display: inline-block;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+`
+
+const CurrentTag = styled.strong`
+  display: inline-block;
+  font-weight: 500;
+
+  &:before {
+    content: '#';
+  }
+`
+
+const TagItemsTemplate: React.FC<PageProps<TagItemsQuery>> = ({ data }) => {
+  const tag = data.contentfulTag
+  const siteMetadata = useSiteMetadata()
+
+  if (!tag) {
+    return null
+  }
+
+  return (
+    <Layout>
+      <SEO title={`Tags / ${tag.name}`} />
+      <Nav>
+        <EffectedLink to={`/${siteMetadata?.tagsPagePath}`}>Tags</EffectedLink>
+        <Separator aria-hidden>/</Separator>
+        <CurrentTag>{tag.name}</CurrentTag>
+      </Nav>
+      {data.allContentfulBlogPost.edges.map(({ node }) =>
+        node ? (
+          <BlogPost
+            key={node.slug}
+            body={node.body || null}
+            date={node.date}
+            permanent={false}
+            pictures={node.pictures || []}
+            slug={node.slug || null}
+            tags={node.tags || []}
+            title={node.title || null}
+          />
+        ) : undefined
+      )}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query TagItems($slug: String!) {
+    contentfulTag(slug: { eq: $slug }) {
+      name
+      slug
+    }
+    allContentfulBlogPost(
+      sort: { fields: date, order: DESC }
+      filter: { tags: { elemMatch: { slug: { eq: $slug } } } }
+    ) {
+      edges {
+        node {
+          title
+          slug
+          date(locale: "ja-jp")
+          pictures {
+            height
+            key
+            width
+          }
+          tags {
+            name
+            slug
+          }
+          body {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default TagItemsTemplate
