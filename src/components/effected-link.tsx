55bb1546c8f2bb2f0a59ae6711257ab1f91f5a96
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-05 17:54:48 +0900

    feat: <EffectedLink />

---
+++ src/components/effected-link.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const EffectedLink = styled(Link).attrs<{ color?: string }>(
+  ({ color = 'currentColor' }) => ({ color })
+)<{ color?: string }>`
+  position: relative;
+  display: inline-block;
+  color: ${({ color }) => color};
+  text-decoration: none;
+
+  &:after {
+    will-change: left, right;
+    content: '';
+    position: absolute;
+    bottom: -1px;
+    left: 50%;
+    right: 50%;
+    height: 1px;
+    background-color: ${({ color }) => color};
+    transition: left 200ms ease-out, right 200ms ease-out;
+  }
+
+  &:hover,
+  &:active,
+  &:focus {
+    &:after {
+      left: 0;
+      right: 0;
+    }
+  }
+`
+
+export default EffectedLink
