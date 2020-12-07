dbedd2e2afda87926fde92918f2cf68cce18f578
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-07 21:31:43 +0900

    fix: <Layout />

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -58,15 +58,15 @@ const Layout: React.FC = ({ children }) => {
     <>
       <SEO>
         <script>{`
-        setTimeout(() => (function(d) {
-          var config = {
-            kitId: "${siteMetadata?.typekitId}",
-            scriptTimeout: 3000,
-            async: true
-          },
-          h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-        })(document), 1);
-      `}</script>
+setTimeout(function() {(function(d) {
+  var config = {
+    kitId: "${siteMetadata?.typekitId}",
+    scriptTimeout: 3000,
+    async: true
+  },
+  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+})(document)}, 1);
+        `}</script>
         <noscript>{`<style type="text/css">#${CONTAINER_ID} { opacity: 1 }</style>`}</noscript>
       </SEO>
       <Container id={CONTAINER_ID}>
