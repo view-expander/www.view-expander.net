a8c77f10fdf07dd09da492b7e3c7735d574afd18
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-08 18:02:07 +0900

    feat: libs/index.ts, `createPhotoPath()`

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,4 +2,7 @@ import ImgixClient from 'imgix-core-js'
 
 const domain = process.env.GATSBY_IMGIX_DOMAIN
 
+export const createPhotoPath = (key: string) =>
+  `${process.env.GATSBY_IMGIX_PATH}/${key}`
+
 export const imgixClient = domain ? new ImgixClient({ domain }) : undefined
