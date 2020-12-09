b02eeba28d554223fbfc28a44d8aeff9195d2ef8
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-09 21:35:45 +0900

    feat: libs, `getImageSources()`

--- src/components/photo-preview.tsx
+++ src/components/photo-preview.tsx
@@ -1,59 +1,28 @@
 import React from 'react'
-import { createPhotoPath, imgixClient } from '../libs'
+import { getImageSources } from '../libs'
 import PhotoImage from './photo-image'
 
 type Props = {
   meta: PhotoMeta
 }
 
-const WIDTHS = [543, 688, 928, 1288]
-
 const PhotoPreview: React.FC<Props> = ({ meta }) => {
-  if (imgixClient) {
-    const { height, key, width } = meta
-    const aspectRatio = height / width
-    const src = imgixClient.buildURL(createPhotoPath(key), {
-      auto: 'format',
-      fit: 'clip',
-      colorquant: 2,
-      sat: -100,
-      gam: -80,
-      blur: 20,
-      w: WIDTHS[WIDTHS.length - 1],
-      h: WIDTHS[WIDTHS.length - 1],
-    })
-    const srcset = imgixClient.buildSrcSet(
-      createPhotoPath(key),
-      {
-        auto: 'format',
-        fit: 'clip',
-        colorquant: 2,
-        sat: -100,
-        gam: -80,
-        blur: 20,
-      },
-      {
-        widths:
-          aspectRatio > 1
-            ? WIDTHS.map(w => Math.ceil(w / aspectRatio))
-            : WIDTHS,
-      }
-    )
+  const { height, key, width } = meta
+  const aspectRatio = height / width
+  const { src, srcSet } = getImageSources(key, aspectRatio, {
+    colorquant: 2,
+    sat: -100,
+    gam: -90,
+    blur: 20,
+  })
 
-    return (
-      <PhotoImage
-        alt=""
-        aria-hidden
-        aspectRatio={aspectRatio}
-        height={height}
-        src={src}
-        srcSet={srcset}
-        width={width}
-      />
-    )
-  }
-
-  return null
+  return (
+    <PhotoImage
+      alt=""
+      aria-hidden
+      {...{ aspectRatio, height, src, srcSet, width }}
+    />
+  )
 }
 
 export default PhotoPreview
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,8 +1,46 @@
 import ImgixClient from 'imgix-core-js'
 
-const domain = process.env.GATSBY_IMGIX_DOMAIN
+const IMGIX_DOMAIN = process.env.GATSBY_IMGIX_DOMAIN
+const IMGIX_COMMON_PARAMS = {
+  auto: 'format',
+  fit: 'clip',
+}
+const WIDTHS = [543, 688, 928, 1288]
 
 export const createPhotoPath = (key: string) =>
   `${process.env.GATSBY_IMGIX_PATH}/${key}`
 
-export const imgixClient = domain ? new ImgixClient({ domain }) : undefined
+export const imgixClient = IMGIX_DOMAIN
+  ? new ImgixClient({ domain: IMGIX_DOMAIN })
+  : undefined
+
+export const getImageSources = (
+  key: string,
+  aspectRatio: number,
+  params: {} = {}
+) => {
+  if (!imgixClient) {
+    return { src: undefined, srcSet: undefined }
+  }
+
+  const path = createPhotoPath(key)
+  const src = imgixClient.buildURL(path, {
+    ...IMGIX_COMMON_PARAMS,
+    w: WIDTHS[WIDTHS.length - 1],
+    h: WIDTHS[WIDTHS.length - 1],
+    ...params,
+  })
+  const srcSet = imgixClient.buildSrcSet(
+    path,
+    {
+      ...IMGIX_COMMON_PARAMS,
+      ...params,
+    },
+    {
+      widths:
+        aspectRatio > 1 ? WIDTHS.map(w => Math.ceil(w / aspectRatio)) : WIDTHS,
+    }
+  )
+
+  return { src, srcSet }
+}
