8d9d3ab46a83d6525c3759dbdcd221ebf4854b91
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-12 14:14:35 +0900

    feat: guards.ts

---
+++ src/libs/guards.ts
@@ -0,0 +1,9 @@
+const getObjectTypeName = (value: unknown): string => {
+  return Object.prototype.toString.call(value)
+}
+
+export const isString = (value: unknown): value is string =>
+  getObjectTypeName(value) === '[object String]'
+
+export const isStringOfNotEmpty = (value: unknown): value is string =>
+  isString(value) && value.length > 0
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,11 +1,2 @@
+export { isString, isStringOfNotEmpty } from './guards'
 export { getPhotoAttributes } from './imgix'
-
-const getObjectTypeName = (value: unknown): string => {
-  return Object.prototype.toString.call(value)
-}
-
-export const isString = (value: unknown): value is string =>
-  getObjectTypeName(value) === '[object String]'
-
-export const isStringOfNotEmpty = (value: unknown): value is string =>
-  isString(value) && value.length > 0
