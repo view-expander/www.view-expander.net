4eac9e70900af0bb6d30175cc8d0ff9ac981bd07
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-12 16:07:23 +0900

    feat: lib, `getPath()`

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,2 +1,14 @@
+import { isStringOfNotEmpty } from './guards'
+
 export { isString, isStringOfNotEmpty } from './guards'
 export { getPhotoAttributes } from './imgix'
+
+type PathSegment = string | null | undefined
+
+export const getPath = (
+  root: PathSegment = '/',
+  ...pathSegments: PathSegment[]
+) =>
+  `${isStringOfNotEmpty(root) ? root : ''}${pathSegments
+    .filter(isStringOfNotEmpty)
+    .join('/')}/`
