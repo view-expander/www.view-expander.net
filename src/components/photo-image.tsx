60587469fbcce370432181901b4081f2451a5dee
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-13 15:29:10 +0900

    feat: <PhotoImage />

--- src/components/photo-image.tsx
+++ src/components/photo-image.tsx
@@ -4,40 +4,45 @@ type ImgAttrs = {
   aspectRatio: number
 }
 
+const getWidthMag = ({ aspectRatio }: ImgAttrs) =>
+  aspectRatio <= 1 ? 1 : 1 / aspectRatio
+const getHeightMag = ({ aspectRatio }: ImgAttrs) =>
+  aspectRatio <= 1 ? aspectRatio : 1
+
 const PhotoImage = styled.img.attrs<ImgAttrs>(attrs => ({
   ...attrs,
   alt: '',
 }))<ImgAttrs>`
   object-fit: contain;
   display: block;
-  max-width: calc(100vw - 2rem);
-  max-height: calc((100vw - 2rem) * ${({ aspectRatio }) => aspectRatio});
+  max-width: calc((100vw - 2rem) * ${getWidthMag});
+  max-height: calc((100vw - 2rem) * ${getHeightMag});
 
   @media (min-width: 576px) {
-    max-width: calc(540px - 2rem);
-    max-height: calc((540px - 2rem) * ${({ aspectRatio }) => aspectRatio});
+    max-width: calc((540px - 2rem) * ${getWidthMag});
+    max-height: calc((540px - 2rem) * ${getHeightMag});
   }
 
   @media (min-width: 768px) {
-    max-width: calc(720px - 2rem);
-    max-height: calc((720px - 2rem) * ${({ aspectRatio }) => aspectRatio});
+    max-width: calc((720px - 2rem) * ${getWidthMag});
+    max-height: calc((720px - 2rem) * ${getHeightMag});
   }
 
   @media (min-width: 992px) {
-    max-width: calc(960px - 2rem);
-    max-height: calc((960px - 2rem) * ${({ aspectRatio }) => aspectRatio});
+    max-width: calc((960px - 2rem) * ${getWidthMag});
+    max-height: calc((960px - 2rem) * ${getHeightMag});
   }
 
   @media (min-width: 1200px) {
-    width: cal(1140px - 2rem);
-    height: calc((1140px - 2rem) * ${({ aspectRatio }) => aspectRatio});
-    max-width: 95vh;
-    max-height: calc(95vh * ${({ aspectRatio }) => aspectRatio});
+    width: 95vh;
+    height: 95vh;
+    max-width: cal((1140px - 2rem) * ${getWidthMag});
+    max-height: cal((1140px - 2rem) * ${getHeightMag});
   }
 
   @media (min-width: 1400px) {
-    width: cal(1320px - 2rem);
-    height: calc((1320px - 2rem) * ${({ aspectRatio }) => aspectRatio});
+    max-width: cal((1320px - 2rem) * ${getWidthMag});
+    max-height: cal((1320px - 2rem) * ${getHeightMag});
   }
 `
 
