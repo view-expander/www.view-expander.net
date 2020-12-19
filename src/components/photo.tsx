35dc859a68360bbd84f95732996eab8271b166a8
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-19 19:53:58 +0900

    feat: <Photo />

--- src/components/photo.tsx
+++ src/components/photo.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useInView } from 'react-intersection-observer'
 import styled from 'styled-components'
@@ -31,21 +31,26 @@ const Photo: React.FC<Props> = ({ meta }) => {
   })
   const hiResPhotoAttrs = getPhotoAttributes(meta)
 
+  const [rect, setRect] = useState({
+    height: hiResPhotoAttrs.height,
+    width: hiResPhotoAttrs.width,
+  })
   const [status, setStatus] = usePhotoStatus()
   const [ref, inView] = useInView({ threshold: 0.33, triggerOnce: true })
   const onLoading = useCallback(() => setStatus(PHOTO_STATUS.LOADING), [
     setStatus,
   ])
   const onLoaded = useCallback(
-    () =>
+    ({ height, width }: Pick<PhotoMeta, 'height' | 'width'>) =>
       requestAnimationFrame(() => {
         const $html = document.querySelector('html')
         const toBeStable =
           $html && $html.getAttribute('data-browser') === 'ie11'
 
-        return setStatus(toBeStable ? PHOTO_STATUS.STABLE : PHOTO_STATUS.LOADED)
+        setRect({ height, width })
+        setStatus(toBeStable ? PHOTO_STATUS.STABLE : PHOTO_STATUS.LOADED)
       }),
-    [setStatus]
+    [setRect, setStatus]
   )
   const onStable = useCallback(
     () => requestAnimationFrame(() => setStatus(PHOTO_STATUS.STABLE)),
@@ -77,10 +82,12 @@ const Photo: React.FC<Props> = ({ meta }) => {
         {inView ? (
           <PhotoHiRes
             {...hiResPhotoAttrs}
+            height={rect.height}
             onLoading={onLoading}
             onLoaded={onLoaded}
             onStable={onStable}
             status={status}
+            width={rect.width}
           />
         ) : undefined}
       </Wrapper>
