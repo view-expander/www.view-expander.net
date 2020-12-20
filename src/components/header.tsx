6cf005305cdaf007e6bc7e59703fc84efa440f12
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-20 12:09:23 +0900

    feat: <Header />

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,19 @@ import styled from 'styled-components'
 import { useSiteMetadata } from '../hooks/useSiteMetadata'
 
 const StyledHeader = styled.header`
+  will-change: opacity;
+  opacity: 0;
   text-align: center;
+  transition: opacity 200ms ease-out;
+
+  .wf-loading & {
+    opacity: 0;
+  }
+
+  .wf-active &,
+  .wf-inactive & {
+    opacity: 1;
+  }
 
   h1 {
     margin-top: 0;
