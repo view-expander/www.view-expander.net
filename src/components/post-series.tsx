0e6610188953e111b8eef41038336811afe6efb1
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-12-30 22:18:22 +0900

    feat: <PostSeries />

--- src/components/post-series.tsx
+++ src/components/post-series.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark'
 import styled from 'styled-components'
+import { useSiteMetadata } from '../hooks/useSiteMetadata'
+import { getPath } from '../libs'
+import EffectedLink from './effected-link'
 
 const P = styled.p`
-  display: flex;
-  align-items: flex-end;
   color: #6c757d;
   line-height: 1rem;
+
+  a {
+    display: flex;
+    align-items: flex-end;
+    color: inherit;
+  }
 
   small {
     font-size: 0.75rem;
@@ -25,12 +32,17 @@ const Icon = styled(CollectionsBookmarkIcon).attrs(attrs => ({
 const PostSeries: React.FC<{ name?: string | null; slug?: string | null }> = ({
   name,
   slug,
-}) =>
-  name && slug ? (
+}) => {
+  const siteMetadata = useSiteMetadata()
+
+  return name && slug ? (
     <P>
-      <Icon />
-      <small>{name}</small>
+      <EffectedLink to={getPath(undefined, siteMetadata?.seriesPagePath, slug)}>
+        <Icon />
+        <small>{name}</small>
+      </EffectedLink>
     </P>
   ) : null
+}
 
 export default PostSeries
